a936b1fb951e7c983a8bc988735d21c68742c59f
gal <galrosenthal@gmail.com>
2019-01-02 01:37:23 +0200

    Create 1 folder named GamePlay is working, Added TODO

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <string.h>
 #include <string>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <random>
+
 #include "Creature.h"
 #include "Zombie.h"
 #include "Wizard.h"
@@ -9,101 +12,123 @@
 #include "Warrior.h"
 #include "Thief.h"
 #include "Necromancer.h"
-//#include "Black_Dragon.h"
+#include "Black_Dragon.h"
+
+
 
 using namespace std;
+struct stat info; // Used to check whether or not folder/file is exists
+
+std::string exec(const char* cmd);
+
+void CreateFolder();
 
 void showAllHeros(Hero& h1);
 
 int main() {
+
+
+
+  cout << "The Game Has Stated " << endl << "And The location of the exe file is:" << endl;
+  CreateFolder();
+
 //    string test;
 //    getline(cin, test);
 //    cout << test;
-    bool tookMyMoney = false, specailTurn = false;
-    int attackMenu, choiceNum, buyMenu;
-    int playerTurnNum;
-    int insertedPlayers = 0;
-    int numOfPlayers;
-    numOfPlayers = 2;
-    Hero *randomTurns[numOfPlayers];
-    for (int i = 0; i < numOfPlayers; ++i) {
-        randomTurns[i] = NULL;
+  bool tookMyMoney = false, specailTurn = false;
+  int attackMenu, choiceNum, buyMenu;
+  int playerTurnNum;
+  int insertedPlayers = 0;
+  int numOfPlayers;
+  numOfPlayers = 2;
+  Hero *randomTurns[numOfPlayers];
+  for (int i = 0; i < numOfPlayers; ++i) {
+      randomTurns[i] = NULL;
     }
-    Thief gal;
-    Thief gal2;
+  Thief gal;
+  Thief gal2;
 //    Necromancer noy;
 //    Necromancer noy2;
 //    Necromancer noy3;
 //    Warrior gal3;
 
-    map<string,Hero*>::iterator it1;
-    it1 = Hero::allHeros.begin();
-    while (insertedPlayers < numOfPlayers && it1 != Hero::allHeros.end()) {
-        playerTurnNum = rand() % numOfPlayers;
-        if (randomTurns[playerTurnNum] == NULL) {
-            randomTurns[playerTurnNum] = &it1->second->getHero();
-            it1++;
-            insertedPlayers++;
+//TODO: Change rand() to random() as in the /**/
+/*
+  long rndom;
+  while (true)
+    {
+      rndom = random ()%10;
+      cout << rndom << endl;
+    }
+    */
+  map<string,Hero*>::iterator it1;
+  it1 = Hero::allHeros.begin();
+  while (insertedPlayers < numOfPlayers && it1 != Hero::allHeros.end()) {
+      playerTurnNum = rand() % numOfPlayers;
+      if (randomTurns[playerTurnNum] == NULL) {
+          randomTurns[playerTurnNum] = &it1->second->getHero();
+          it1++;
+          insertedPlayers++;
         }
     }
 
 
-    playerTurnNum = 0;
-    cout << "Wellcome <userName" << endl;
-    while (1) {
-        // print menu
-        cout << "What is your next step in the path to victory?\n";
-        cout << "1. Attack\n";
-        cout << "2. Get daily gold\n";
-        cout << "3. Buy creatures\n";
-        cout << "4. Show details\n";
-        cout << "5. Spacial skill\n";
-        cout << "6. End of my turn\n";
-        cout << "7. Exit\n";
-        // get input from user
-        cin >> choiceNum;
-        // take action
-        switch (choiceNum) {
-            case 1:
-                cout << "1. Show me my opponents\n";
-                cout << "2. Attack hero\n";
-                cin >> attackMenu;
-                switch (attackMenu) {
-                    case 1:
-                        showAllHeros(randomTurns[playerTurnNum]->getHero());
-                        break;
-                    case 2:
-                        if(randomTurns[playerTurnNum]->getHero().attackOpponent()) {
-                                if (playerTurnNum + 1 >= numOfPlayers) {
-                                    playerTurnNum = 0;
-                                } else {
-                                    playerTurnNum++;
-                                }
-                                tookMyMoney = false;
-                                specailTurn = false;
-                            }
+  playerTurnNum = 0;
+  cout << "Wellcome <userName" << endl;
+  while (1) {
+      // print menu
+      cout << "What is your next step in the path to victory?\n";
+      cout << "1. Attack\n";
+      cout << "2. Get daily gold\n";
+      cout << "3. Buy creatures\n";
+      cout << "4. Show details\n";
+      cout << "5. Spacial skill\n";
+      cout << "6. End of my turn\n";
+      cout << "7. Exit\n";
+      // get input from user
+      cin >> choiceNum;
+      // take action
+      switch (choiceNum) {
+          case 1:
+            cout << "1. Show me my opponents\n";
+          cout << "2. Attack hero\n";
+          cin >> attackMenu;
+          switch (attackMenu) {
+              case 1:
+                showAllHeros(randomTurns[playerTurnNum]->getHero());
+              break;
+              case 2:
+                if(randomTurns[playerTurnNum]->getHero().attackOpponent()) {
+                    if (playerTurnNum + 1 >= numOfPlayers) {
+                        playerTurnNum = 0;
+                      } else {
+                        playerTurnNum++;
+                      }
+                    tookMyMoney = false;
+                    specailTurn = false;
+                  }
 
-                        break;
-                    default:
-                        cout << "please choose a valid number\n";
-                }
-                break;
-            case 2:
-                if(!tookMyMoney)
-                {
-                    randomTurns[playerTurnNum]->increaseGold(100);
-                    tookMyMoney = true;
-                }
-                break;
-            case 3:
-                randomTurns[playerTurnNum]->buyCreatures();
-                break;
-            case 4:
-                randomTurns[playerTurnNum]->heroDetails();
-                break;
-            case 5:
-                if(!specailTurn)
-                {
+              break;
+              default:
+                cout << "please choose a valid number\n";
+            }
+          break;
+          case 2:
+            if(!tookMyMoney)
+              {
+                randomTurns[playerTurnNum]->increaseGold(100);
+                tookMyMoney = true;
+              }
+          break;
+          case 3:
+            randomTurns[playerTurnNum]->buyCreatures();
+          break;
+          case 4:
+            randomTurns[playerTurnNum]->heroDetails();
+          break;
+          case 5:
+            if(!specailTurn)
+              {
 //                    try{
 //
 //                    }
@@ -111,62 +136,126 @@ int main() {
 //                    {
 //
 //                    }
-                    randomTurns[playerTurnNum]->specialSkill();
-                    specailTurn = true;
-                }
-                break;
-            case 6:
-                if(playerTurnNum + 1 >= numOfPlayers)
-                {
-                    playerTurnNum = 0;
-                } else
-                {
-                    playerTurnNum++;
-                }
-                tookMyMoney = false;
-                specailTurn = false;
-                break;
-            case 7:
-                exit(0);
-            default:
-                cout << ("please choose a valid number\n");
+                randomTurns[playerTurnNum]->specialSkill();
+                specailTurn = true;
+              }
+          break;
+          case 6:
+            if(playerTurnNum + 1 >= numOfPlayers)
+              {
+                playerTurnNum = 0;
+              } else
+              {
+                playerTurnNum++;
+              }
+          tookMyMoney = false;
+          specailTurn = false;
+          break;
+          case 7:
+            exit(0);
+          default:
+            cout << ("please choose a valid number\n");
         }
     }
 
-        int x; // for BreakPoint
-        return 0;
+  int x; // for BreakPoint
+  return 0;
 }
 
 
 void showAllHeros(Hero& h1)
 {
 
-    map<string,Hero*>::iterator it1;
-    it1 = Hero::allHeros.begin();
-    while (it1 != Hero::allHeros.end() )
+  map<string,Hero*>::iterator it1;
+  it1 = Hero::allHeros.begin();
+  while (it1 != Hero::allHeros.end() )
     {
-        if(!(h1.getName() == it1->first)&& it1->second->getType() == "Warrior")
+      if(!(h1.getName() == it1->first)&& it1->second->getType() == "Warrior")
         {
-            it1->second->printNameType();
+          it1->second->printNameType();
         }
-        it1++;
+      it1++;
     }
-    it1 = Hero::allHeros.begin();
-    while (it1 != Hero::allHeros.end() )
+  it1 = Hero::allHeros.begin();
+  while (it1 != Hero::allHeros.end() )
     {
-        if(!(h1.getName() == it1->first)&& it1->second->getType() == "Thief")
+      if(!(h1.getName() == it1->first)&& it1->second->getType() == "Thief")
         {
-            it1->second->printNameType();
+          it1->second->printNameType();
         }
-        it1++;
+      it1++;
     }
-    it1 = Hero::allHeros.begin();
-    while (it1 != Hero::allHeros.end() )
+  it1 = Hero::allHeros.begin();
+  while (it1 != Hero::allHeros.end() )
     {
-        if(!(h1.getName() == it1->first)&& it1->second->getType() == "Necromancer")
+      if(!(h1.getName() == it1->first)&& it1->second->getType() == "Necromancer")
         {
-            it1->second->printNameType();
+          it1->second->printNameType();
         }
-        it1++;
+      it1++;
     }
 }
+
+//TODO: Maybe we should Recursivly create the folder inside GamePlay
+/***
+ * Creates the folder of the game
+ *
+ *
+ */
+void CreateFolder ()
+{
+  int check;
+  char *dirName;
+
+  dirName = "./GamePlay";
+  if (stat (dirName, &info) != 0)
+    {
+      // Folder does not exists
+      check = mkdir (dirName, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+      if (-1 == check)
+        {
+          // Could not create the folder
+        }
+      else
+        {
+          // Created the new Folder
+          // Create the Files of the Useres Now
+          cout << exec ("pwd") << endl;
+        }
+
+    }
+  else if (info.st_mode & S_IFDIR)
+    {
+      // it is a directory and it is already created
+      // Create the Files of the Users Now
+      cout << exec ("pwd") << endl;
+    }
+  else
+    {
+      // Not A directory and cannot run the Program
+      exit(1);
+    }
+
+
+
+
+}
+
+std::string exec (const char *cmd)
+{
+  char buffer[128];
+  std::string result = "";
+  FILE* pipe = popen(cmd, "r");
+  if (!pipe) throw std::runtime_error("popen() failed!");
+  try {
+      while (fgets(buffer, sizeof buffer, pipe) != NULL) {
+          result += buffer;
+        }
+    } catch (...) {
+      pclose(pipe);
+      throw;
+    }
+  pclose(pipe);
+  return result;
+}
+
